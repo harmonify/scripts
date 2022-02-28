3bf15e4a2b4b40d33995ebed1d8605b0e15c09ae
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-02-28 20:34:53 +0700

    improve logging

--- pylink.cmd
+++ pylink.cmd
@@ -1,9 +1,13 @@
-@REM by Amr Ali on https://stackoverflow.com/a/27083780
+@REM by Amr Ali on https://stackoverflow.com/a/27083780\
+
+@echo off
+echo Running pylink.cmd as elevated user...
+echo Log file    : %~dp0pylink.log
+echo.
 <!-- : --- Self-Elevating Batch Script ---------------------------
 @whoami /groups | find "S-1-16-12288" > nul && goto :admin
 set "ELEVATE_CMDLINE=cd /d "%~dp0" & call "%~f0" %*"
 cscript //nologo "%~f0?.wsf" //job:Elevate & exit /b
-
 -->
 <job id="Elevate"><script language="VBScript">
   Set objShell = CreateObject("Shell.Application")
@@ -13,14 +17,9 @@ cscript //nologo "%~f0?.wsf" //job:Elevate & exit /b
   objShell.ShellExecute "cmd", "/c " & strCommandLine, "", "runas"
 </script></job>
 :admin -----------------------------------------------------------
-
-@echo off
-echo Running as elevated user.
-echo Script file : %~f0
-echo Arguments   : %*
-echo Working dir : %cd%
-echo Log file: "pylink.log"
-echo.
 :: administrator commands here
-:: e.g., run shell as admin and echo output with timestamp to pylink.log
-cmd /c python pylink %* >> "pylink.log"
+:: run command and set it to %output% variable
+for /f "delims=" %%i in ('cmd /k python pylink %*') do set output=%%i
+:: then echo %output% with timestamp to pylink.log
+echo %date% %time% %output% >> %~dp0pylink.log
+exit /b
